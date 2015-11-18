aa691ce40e09563fa502bc457935daf1523dcae2
Khorshed Alam <thekhorshed.alam@gmail.com>
2015-11-19 00:08:12 +0600

    add basic script file

---
+++ app/scripts/script.js
@@ -0,0 +1,15 @@
+// IIFE with jQuery Wrapper
+(function($) {
+  "use strict";
+
+  /*
+   *----------------------------------
+   * Document Ready
+   *----------------------------------
+   */
+  $(document).ready(function() {
+
+
+  });// DOM Ready
+
+}(jQuery)); // IIFE
